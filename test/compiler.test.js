a3cb5045c54ce3400e38609f424775f11e887c65
abmarz <aalmarzooq04@gmail.com>
2025-02-22 20:48:42 -0800

    phase one of Boum

---
+++ test/compiler.test.js
@@ -0,0 +1,8 @@
+import { deepEqual } from 'node:assert/strict';
+import { describe, it } from 'node:test';
+
+describe('Compiler', () => {
+    it('is alive', () => {
+        deepEqual(1, 1);
+    });
+});
