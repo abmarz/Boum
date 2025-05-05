c6b63dabdc166cfdb8707529a1372b1ebc6e3626
abmarz <aalmarzooq04@gmail.com>
2025-05-05 16:42:05 -0700

    office hours 2

--- src/compiler.js
+++ src/compiler.js
@@ -1,4 +1,17 @@
-export default function compile(sourceCode) {
-    return "it works";
-}
+import analyze from "./analyzer.js";
+import generate from "./generator.js";
+import optimize from "./optimizer.js";
+import parse from "./parser.js";
 
+export default function compile(source, outputType) {
+  if (!["parsed", "analyzed", "optimized", "js"].includes(outputType)) {
+    throw new Error("Unknown output type");
+  }
+  const match = parse(source);
+  if (outputType === "parsed") return "Syntax is ok";
+  const analyzed = analyze(match);
+  if (outputType === "analyzed") return analyzed;
+  const optimized = optimize(analyzed);
+  if (outputType === "optimized") return optimized;
+  return generate(optimized);
+}
