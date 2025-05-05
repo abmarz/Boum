82ed3f74b109d72eb5455cd7b465c14aaf76319f
abmarz <aalmarzooq04@gmail.com>
2025-05-05 16:48:42 -0700

    update

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,18 +1,9 @@
-// import { deepEqual } from 'node:assert/strict';
-// import { describe, it } from 'node:test';
+import assert from "node:assert";
+import { describe, it } from "node:test";
+import compile from "../src/compiler.js";
 
-// describe('Compiler', () => {
-//     it('is alive', () => {
-//         deepEqual(1, 1);
-//     });
-// });
-
-import assert from 'node:assert';
-import { describe, it } from 'node:test';
-import compile from '../src/compiler.js';
-
-describe ("The compiler", () => {
-    it("is alive", () => {
-        assert.equal(compile('print "Hello'), "it works");
-    });
+describe("The compiler", () => {
+  it("is alive", () => {
+    assert.equal(compile('print "Hello'), "it works");
+  });
 });
